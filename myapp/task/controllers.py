38c2d6b5bf67820fd9d48824df1e9da5cef3acd0
FanaticJungle <josuefariasv@gmail.com>
2024-11-21 10:09:59 -0600

    Creacion de la tabla task y operaciones tipo CRUD

--- myapp/task/controllers.py
+++ myapp/task/controllers.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from myapp.task import operationsCRUD
 
 taskRoute = Blueprint('task', __name__, url_prefix='/task')#se liga task con el nombre que pone el usuario con la carpeta
 task_list = ['tasks 1', 'tasks 2', 'tasks 3']
 #Creacion de rutas en el modulo rutas, todas estas son rutas
 @taskRoute.route('/')#aquí muestra todo el contenido de la tabla
 def index():
+    #print(operationsCRUD.getById(2).name)
+    #print(operationsCRUD.getAll()[1].name)
+    #print(operationsCRUD.delete(1))
+    print(operationsCRUD.pagination().items)#haciendo consulta a base de datos
     return render_template('dashboard/task/index.html', tasks=task_list)
 
 @taskRoute.route('/<int:id>')#Solo muestra la que tenga este id previo seleccionado
@@ -13,7 +18,7 @@ def show(id:int):
 
 @taskRoute.route('/delete/<int:id>')#aquí podemos meter la función de borrar
 def delete(id:int):
-        if id is not None and id is not "":
+        if id != None and id != "":
             del task_list[id]
             return redirect(url_for('task.index'))
 
@@ -21,7 +26,7 @@ def delete(id:int):
 @taskRoute.route('/create', methods=('GET','POST'))#Aquí se puede meter la función crear
 def create():
     task = request.form.get('task')
-    if task is not None and task is not "":#verifica que la ruta no esté vacía
+    if task != None and task != "":#verifica que la ruta no esté vacía
         task_list.append(task)
         return redirect(url_for('task.index'))
     return render_template('dashboard/task/create.html')
@@ -29,7 +34,7 @@ def create():
 @taskRoute.route('/update/<int:id>', methods=['GET','POST'])#Aquí se actualiza un contenido en específico
 def update(id:int):
     task = request.form.get('task')
-    if task is not None and task is not "":
+    if task != None and task != "":
         task_list[id] = task
         return redirect(url_for('task.index'))
         
