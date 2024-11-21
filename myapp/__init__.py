88a6b1a56eb9f50663127aed25cdc158ebae4cce
FanaticJungle <josuefariasv@gmail.com>
2024-11-21 11:06:47 -0600

    Creación de la migración

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -5,6 +5,7 @@
 
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy#sirve para maejar bases de datos
+from flask_migrate import Migrate
 
 from myapp.config import DevConfig
 app = Flask(__name__)
@@ -14,11 +15,12 @@ app.config.from_object(DevConfig)
 
 #Para la configuracion de la BD
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
 from myapp.task.controllers import taskRoute
 app.register_blueprint(taskRoute)
 #Para la creacion de las tablas en la base de datos
-with app.app_context():
-    db.create_all()
+
 
 @app.route('/')
 def hello_world() -> str:
